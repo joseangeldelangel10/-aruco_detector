3833b22abd27dfbe6fc7ee1baf2c8c0902a36704
Jose del A <joseangeldelangel10@gmail.com>
2022-09-11 01:56:08 -0700

    multiple codes detection enabled

--- aruco_detector.py
+++ aruco_detector.py
@@ -1,52 +1,56 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
-arucoDict = cv.aruco.Dictionary_get(cv.aruco.DICT_4X4_50)
-arucoParams = cv.aruco.DetectorParameters_create()
+def draw_rectangle(image,rectangle_corners):
+	image = cv.line(image, rectangle_corners[0,:].astype(int), rectangle_corners[1,:].astype(int) , (255,0,0), 2)
+	image = cv.line(image, rectangle_corners[1,:].astype(int), rectangle_corners[2,:].astype(int) , (255,0,0), 2)
+	image = cv.line(image, rectangle_corners[2,:].astype(int), rectangle_corners[3,:].astype(int) , (255,0,0), 2)
+	image = cv.line(image, rectangle_corners[3,:].astype(int), rectangle_corners[0,:].astype(int) , (255,0,0), 2)
+	return image
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+def get_rectangle_max_and_mins(rectangle_corners):
+	x_vals = rectangle_corners[:,0]
+	y_vals = rectangle_corners[:,1]
+	return map(int, (min(x_vals), max(x_vals), min(y_vals), max(y_vals)) )
 
-i = 0
-while True:
-	ret, frame = cap.read()
-
-	if not ret:
-		print("Can't receive frame (stream end?). Exiting ...")
-		break
-	# Display the resulting frame
-	# gray_frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-	(corners, ids, rejected) = cv.aruco.detectMarkers(frame, arucoDict,parameters=arucoParams)
-	
-	if (i >= 100 and len(corners) > 0):
-		print("type: {corners_type}".format(corners_type=type(corners[0])))
-		print(" ============================== corners ==============================")
-		print(corners[0])
-		for rectangle in corners[0]:
-			print(" ============================== rectangle ==============================")
-			print(rectangle)
-			#frame = cv.rectangle(frame, rectangle[0,:], rectangle[3,:], (255,0,0), 2)
-		i = 0
-	else:
-		i += 1
-	
+def detect_all_arucos_in_scene(scene, scene_with_hidden_arucos, arucoDict, params):
+	(corners, ids, rejected) = cv.aruco.detectMarkers(scene_with_hidden_arucos, arucoDict,parameters=params)
 	if len(corners) > 0:
 		for rectangle in corners[0]:
-			frame = cv.line(frame, rectangle[0,:].astype(int), rectangle[1,:].astype(int) , (255,0,0), 2)
-			frame = cv.line(frame, rectangle[1,:].astype(int), rectangle[2,:].astype(int) , (255,0,0), 2)
-			frame = cv.line(frame, rectangle[2,:].astype(int), rectangle[3,:].astype(int) , (255,0,0), 2)
-			frame = cv.line(frame, rectangle[3,:].astype(int), rectangle[0,:].astype(int) , (255,0,0), 2)
-	
-	cv.imshow('original frame', frame)
-	
-	if cv.waitKey(1) == ord('q'):
-		break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+			scene = draw_rectangle(scene, rectangle)
+			(min_x, max_x, min_y, max_y) = get_rectangle_max_and_mins(rectangle)
+			white_rect = np.ones((max_y-min_y, max_x-min_x), dtype=np.uint8)*255
+			white_rect = cv.cvtColor(white_rect, cv.COLOR_GRAY2BGR) 
+			scene_with_hidden_arucos[min_y:max_y, min_x:max_x] = white_rect
+		return detect_all_arucos_in_scene(scene, scene_with_hidden_arucos, arucoDict, params)
+	else:
+		return scene
+
+def aruco_detection():
+	cap = cv.VideoCapture(0)
+	arucoDict = cv.aruco.Dictionary_get(cv.aruco.DICT_4X4_50)
+	arucoParams = cv.aruco.DetectorParameters_create()
+
+	if not cap.isOpened():
+		print("Cannot open camera")
+		exit()
+
+	while True:
+		ret, frame = cap.read()
+
+		if not ret:
+			print("Can't receive frame (stream end?). Exiting ...")
+			break
+		
+		frame = detect_all_arucos_in_scene(frame, frame.copy(), arucoDict, arucoParams)
+		cv.imshow('resulting frame', frame)
+		
+		if cv.waitKey(1) == ord('q'):
+			break
+			
+	cap.release()
+	cv.destroyAllWindows()
 
 
-#if __name__ == "__main__":
-#    aruco_detection()
+if __name__ == "__main__":
+    aruco_detection()
